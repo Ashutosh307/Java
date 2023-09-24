35e992a4b2f5493bf29c04af2a77f089216d6d0e
Shakya Ashutosh Sunilbhai <shakyaashutosh80@gmail.com>
2023-09-24 22:33:16 +0530

    Initial Commit

---
+++ Add/src/Sum.java
@@ -0,0 +1,17 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		
+		a = 23;
+		b = 34;
+		c = a + b;
+		
+		System.out.println(c);
+
+	}
+
+}
